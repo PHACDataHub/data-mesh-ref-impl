3294372f4128064a992cc37b4b2d786eafe9ccc6
Nghia Doan <nghia.dh@mac.com>
2023-10-19 20:14:00 -0700

    Completed EHR Event AVRO & csv2avro

--- paradire/stream_pt_ehr_events.sh
+++ paradire/stream_pt_ehr_events.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-valid_pt="AB BC MB NB NL NS NT NU ON PE QC SK YT"
-
 if [ -z "$1" ]; then
-    echo "Usage: ./stream_pt_ehr_events.sh <province_or_territory_abbreviation>"
-    echo "   where province_or_territory_abbreviation is one of: "
-    echo "Example: ./build_image.sh dedup_by_id workflow.yaml main conf/std-requirements.txt conf/avro"
-    exit
-
+    echo "Usage: ./stream_pt_ehr_events.sh <data_dir>"
+    echo "   where data_dir would be, e.g.: "
+    echo "Example: ./stream_pt_ehr_events.sh ~/synthea/ca_spp/SK/avro/2023_10_17T22_19_30Z"
     exit 1
 fi
 
-pt=$1
+set -e
 
 curr_dir=$(pwd)
-cd ~/synthea
-data_dir=$(pwd)/
+avro_dir=${curr_dir}/governance/events
+data_dir=$1
 
 source .env
-
-topic=factiva-articles
-
-avro_dir=${curr_dir}/governance/events
-data_dir=${curr_dir}/data
-tar_file=factiva_5k_avro.tar.gz
-data_file=factiva-articles.txt
-no_messages=4825
 
 broker_container_name=broker
 broker_internal_host=broker
@@ -48,23 +36,26 @@ fi
 echo ''
 
 ./scripts/get_schema_registry_config.sh
-
 ./scripts/list_subjects.sh
 
-./scripts/create_subject.sh ${topic}-key ${avro_dir}/unique-article-key.avsc
-./scripts/create_subject.sh ${topic}-value ${avro_dir}/unique-article-val.avsc
+for topic in allergies careplans claims claims_transactions conditions devices encounters imaging_studies immunizations medications observations organizations patient_expenses patient-key patients payer_transitions payers procedures providers supplies
+do
+    ./scripts/create_subject.sh ${topic}-key ${avro_dir}/ehr_key.avsc
+    ./scripts/create_subject.sh ${topic}-value ${avro_dir}/${topic}.avsc
+
+    ./scripts/get_subject_info.sh ${topic}-key
+    ./scripts/get_subject_info.sh ${topic}-value
+
+    key_schema_id=$(curl --silent -X GET http://${schema_registry_local_host}:${schema_registry_port}/subjects/${topic}-key/versions/latest | jq .id)
+    value_schema_id=$(curl --silent -X GET http://${schema_registry_local_host}:${schema_registry_port}/subjects/${topic}-value/versions/latest | jq .id)
+
+    data_file=${topic}.avro
+    no_messages=$(cat $data_dir/$data_file | wc -l | tr -d ' ')
+
+    echo "Produce ${no_messages} messages ..." 
+    docker exec ${schema_registry_container} bash -c \
+        "kafka-avro-console-producer  --broker-list $broker_internal_host:$broker_internal_port --topic $topic --property key.separator='|' --property parse.key=true --property key.schema.id=$key_schema_id --property value.schema.id=$value_schema_id < /data/$data_file"
+    echo ''
+done
 
 ./scripts/list_subjects.sh
-
-./scripts/get_subject_info.sh ${topic}-key
-./scripts/get_subject_info.sh ${topic}-value
-
-tar xzvf $tar_file -C kafka-ce/schema-registry/data/.
-
-key_schema_id=$(curl --silent -X GET http://${schema_registry_local_host}:${schema_registry_port}/subjects/${topic}-key/versions/latest | jq .id)
-value_schema_id=$(curl --silent -X GET http://${schema_registry_local_host}:${schema_registry_port}/subjects/${topic}-value/versions/latest | jq .id)
-
-echo "Produce ${no_messages} messages ..." 
-docker exec ${schema_registry_container} bash -c \
-    "kafka-avro-console-producer  --broker-list $broker_internal_host:$broker_internal_port --topic $topic --property key.separator='|' --property parse.key=true --property key.schema.id=$key_schema_id --property value.schema.id=$value_schema_id < /data/$data_file"
-echo ''
