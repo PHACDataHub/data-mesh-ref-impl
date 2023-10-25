321aae50b0fac6903475a1ddc457d34edcd76b4b
Nghia Doan <nghia.doan@phac-aspc.gc.ca>
2023-10-25 14:33:18 +0000

    Fix csv_dir

--- paradire/convert_ehr_to_avro.sh
+++ paradire/convert_ehr_to_avro.sh
@@ -3,16 +3,13 @@
 set -e
 
 if [ -z "$1" ]; then
-    if [ -d "data" ]; then
-        data_dir=data
-    else
-        echo "Usage: ./convert_ehr_to_avro.sh <csv_dir> <avro_dir>"
-        echo "Example: ./convert_ehr_to_avro.sh ~/synthea/ca_spp/AB/csv/2023_10_19T18_15_33Z data"
-        exit 1
-    fi
-else
-    data_dir=$1
+    echo "Usage: ./convert_ehr_to_avro.sh <csv_dir> <avro_dir>"
+    echo "Example: ./convert_ehr_to_avro.sh ~/synthea/ca_spp/AB/csv/2023_10_19T18_15_33Z data"
+    exit 1
 fi
 
+if [ ! -d "data" ]; then
+    mkdir data
+fi
 
 python src/csv2avro.py governance/events $1 data
