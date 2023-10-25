1a7b69435773312faf80a1763970daf003cee738
Nghia Doan <nghia.doan@phac-aspc.gc.ca>
2023-10-25 22:46:28 +0000

    Added python installation

--- paradire/convert_ehr_to_avro.sh
+++ paradire/convert_ehr_to_avro.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# if [ -d "~/3.10" ]; then
+#     sudo apt install python3-venv -y
+#     python3 -m venv ~/3.10
+#     source ~/3.10/bin/activate
+# fi
+
 if [ -z "$1" ]; then
     echo "Usage: ./convert_ehr_to_avro.sh <csv_dir> <avro_dir>"
     echo "Example: ./convert_ehr_to_avro.sh ~/synthea/ca_spp/AB/csv/2023_10_19T18_15_33Z data"
