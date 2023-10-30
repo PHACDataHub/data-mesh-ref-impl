d1aee17090b45c3498022ec4dbd44a55e4fa87a0
Luc Belliveau <luc.belliveau@phac-aspc.gc.ca>
2023-10-30 14:21:02 +0000

    chore: updated script for latest version of patient browser

--- paradire/setup_patient_browser.sh
+++ paradire/setup_patient_browser.sh
@@ -15,22 +15,31 @@ fi
 cd 
 
 if [ ! -d "patient-browser" ]; then
+    sudo npm install typescript -g
     git clone https://github.com/smart-on-fhir/patient-browser.git
     ## Install dependencies and build project
-    (cd patient-browser && NODE_ENV=production npm ci)
+    (cd patient-browser && NODE_ENV=production npm install colors request commander@2.15.1 && npm ci && NODE_ENV=production npm run build)
 fi
 
 cd patient-browser
 
 # Configure browser
-rm -f ./build/config/*.json5
+rm -f ./dist/config/*.json5
 mkdir ./config
 echo "Generating config, please wait..."
-node config-genrator/generate_config.js -s http://127.0.0.1:8080/fhir -f default
-mv config/default.json5 ./build/config/
+node config-genrator/generate_config.js --server http://127.0.0.1:8080/fhir --file default
+sleep 1
+mv config/default.json5 ./dist/config/
 rm -rf ./config
 # Disable the broken fihr-viewer
-sed -i 's/enabled: true/enabled: false/g' ./build/config/default.json5
+sed -i 's/enabled: true/enabled: false/g' ./dist/config/default.json5
+
+# Create a dockerfile
+cat <<EOL > Dockerfile
+FROM nginx:alpine
+COPY ./dist /usr/share/nginx/html
+CMD ["sh", "-c", "nginx -g 'daemon off;'"]
+EOL
 
 # Build docker image
 docker build . -t patient-browser
