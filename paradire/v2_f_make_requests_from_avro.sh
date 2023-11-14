620d4ca6f29cb19ab4ef30cd20add0a280cf2995
Nghia Doan <nghia.dh@mac.com>
2023-11-13 17:20:23 -0800

    Refactored Fed Requests into FAR-1 to -9

--- paradire/v2_f_make_requests_from_avro.sh
+++ paradire/v2_f_make_requests_from_avro.sh
@@ -40,30 +40,3 @@ do
         "kafka-avro-console-producer  --broker-list $broker_internal_host:$broker_internal_port --topic $request_topic --property key.separator='|' --property parse.key=true --property key.schema.id=$key_schema_id --property value.schema.id=$value_schema_id < /data/$data_file"
     echo ''
 done
-
-timeout=30000
-
-for item in {1..9}
-# for item in 1
-do
-    response_topic=fas_${item}
-    
-    consumer_group=${response_topic}
-
-    echo "Consume up to ${response_messages} messages from ${response_topic} ..." 
-    docker exec -it ${schema_registry_container} kafka-avro-console-consumer  \
-        --bootstrap-server ${broker_internal_host}:${broker_internal_port} \
-        --topic ${response_topic} --group ${consumer_group} --from-beginning --timeout-ms ${timeout}\
-        --property schema.registry.url=http://${schema_registry_internal_host}:${schema_registry_port}
-    echo ''
-    echo Reset all consumer offsets of ${consumer_group} consumer group ...
-    echo "docker exec -it ${broker_container_name} /bin/kafka-consumer-groups \
-        --bootstrap-server ${broker_internal_host}:${broker_internal_port} \
-        --group ${consumer_group} --reset-offsets --to-earliest --all-topics --execute;
-    "
-    docker exec -it ${broker_container_name} /bin/kafka-consumer-groups \
-        --bootstrap-server ${broker_internal_host}:${broker_internal_port} \
-        --group ${consumer_group} --reset-offsets --to-earliest --all-topics --execute;
-    echo "Consumer offsets reset ✅";
-    echo ''
-done
