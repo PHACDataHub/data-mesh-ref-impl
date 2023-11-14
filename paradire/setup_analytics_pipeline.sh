e20bf552ef1786a9f963f5cc52b667b81937ef15
Nghia Doan <nghia.doan@phac-aspc.gc.ca>
2023-11-14 17:46:09 +0000

    Remove v1 code

--- paradire/setup_analytics_pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source .env
-
-echo 'Creating constraints and indexes ...'
-sudo cp analytics/neo4j/entity_constraints.cql neo4j/import/.
-docker exec -u ${NEO4J_USERNAME} --interactive --tty  neo4j cypher-shell -u ${NEO4J_USERNAME} -p ${NEO4J_PASSWORD} --file /import/entity_constraints.cql
-echo 'Constraints and indexes are created ✅'
