ad8846ef5539fe3f7de63f776d96b8773080f3b8
simardeepsingh <simardeep.oberoi@gmail.com>
2023-11-20 09:44:49 -0500

    Helm command changes

--- paradire/k8s/create-pt-cluster.sh
+++ paradire/k8s/create-pt-cluster.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-
 provinces="ab bc mb nb nl ns nt nu on pe qc sk yt"
+selected_provinces=($provinces) # Automatically select all 13 provinces
 
-# Get user input for number of records
+# Get user input for action: 1 for install, 2 for upgrade
+echo "Choose an action: 1 for install, 2 for upgrade"
+read action_choice
+
+# Validate action input
+if ! [[ "$action_choice" == "1" || "$action_choice" == "2" ]]; then
+    echo "Invalid choice. Please enter '1' for install or '2' for upgrade."
+    exit 1
+fi
+
+# Get user input for number of records in every case
 read -p "Enter the number of records you want to generate and upload: " record_count
 
 # Validate record count
 if ! [[ "$record_count" =~ ^[0-9]+$ ]]; then
-    log "Invalid record count. Please enter a valid number."
+    echo "Invalid record count. Please enter a valid number."
     exit 1
 fi
 
-# Ask the user for provinces
-echo "Enter the province abbreviations separated by space (e.g., ab bc mb nb nl ns nt nu on pe qc sk yt). Type 'all' for all provinces."
-read -a selected_provinces
-
-if [[ "${selected_provinces[0]}" == "all" ]]; then
-    selected_provinces=($provinces)
-fi
-
-# Sequentially generate and upload data for the selected provinces
+# Sequentially process data for all provinces
 for pt in "${selected_provinces[@]}"; do
-    if [[ ! " $provinces " =~ " $pt " ]]; then
-        log "Invalid province abbreviation: $pt. Skipping."
-        continue
+    # Determine whether to install or upgrade
+    if [[ "$action_choice" == "1" ]]; then
+        helm install "$pt"-province . --namespace="$pt" \
+        --create-namespace \
+        --set cp-kafka-ui.paradire.pt="$pt" \
+        --set cp-kafka-job.samplingSize="$record_count" \
+        --set cp-kafka-job.pt="$pt" \
+        --set cp-kafka-job.GCPBucketName="paradire-synthea-data" || exit $?
+    elif [[ "$action_choice" == "2" ]]; then
+        helm upgrade "$pt"-province . --namespace="$pt" \
+        --set cp-kafka-ui.paradire.pt="$pt" \
+        --set cp-kafka-job.samplingSize="$record_count" \
+        --set cp-kafka-job.pt="$pt" \
+        --set cp-kafka-job.GCPBucketName="paradire-synthea-data" || exit $?
     fi
-
-    helm install "$pt"-province . --namespace="$pt" \
-    --create-namespace \
-    --set cp-kafka-ui.paradire.pt="$pt" \
-    --set cp-kafka-job.samplingSize="$record_count" \
-    --set cp-kafka-job.pt="$pt" \
-    --set cp-kafka-job.GCPBucketName="paradire-synthea-data" || exit $?
-    
 done
 
 echo "All tasks completed!"
