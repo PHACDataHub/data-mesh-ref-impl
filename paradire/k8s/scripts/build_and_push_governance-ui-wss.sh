44718cdc60080f560c43b631a7520c74ec20844f
Luc Belliveau <luc.belliveau@phac-aspc.gc.ca>
2023-11-28 17:41:13 +0000

    chore: added script to build and push the governance ui wss server

---
+++ paradire/k8s/scripts/build_and_push_governance-ui-wss.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Constants
+LOCATION="northamerica-northeast1-docker.pkg.dev"
+PROJECT="phx-01he5rx4wsv"
+REGISTRY="paradire"
+
+DOCKER_IMG_NAME="governance-ui-wss"
+TAG="latest"
+
+# Setup docker authentication
+sudo docker login -u oauth2accesstoken -p "$(gcloud auth print-access-token)" https://$LOCATION
+
+IMAGE="$LOCATION/$PROJECT/$REGISTRY/$DOCKER_IMG_NAME:$TAG"
+
+# Build and push Docker image
+sudo docker build . -f ./governance/governance-ui/Dockerfile.wsserver -t "$IMAGE"
+sudo docker push $IMAGE
